d4e22fc2323e1a75c683a843fa30f83c8cb5f4a5
Shokh Abbos Ulugmurodov <71746304+shohabbosdev@users.noreply.github.com>
2026-01-07 23:07:29 +0500

    yonma yon qilindi

--- src/App.tsx
+++ src/App.tsx
@@ -88,9 +88,11 @@ function App() {
         <div className="container mx-auto px-4">
           <div className="max-w-4xl mx-auto">
             <div className="grid grid-cols-1 gap-8">
-              <BotConfig config={config} onConfigChange={setConfig} />
-              
-              <FileUpload config={config} />
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+                <BotConfig config={config} onConfigChange={setConfig} />
+                
+                <FileUpload config={config} />
+              </div>
               
               <ExcelExample />
             </div>
